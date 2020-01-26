c3b0b9ab1ae87dfb35b3e22a96ad527d6265dff9
bharatnandini <bharatnandini16@gmail.com>
2020-01-26 12:49:23 +0530

    my first commit test

---
+++ Files!/var.js
@@ -0,0 +1,26 @@
+// let var1 = 2000;
+// let var2 = 8000;
+
+//function definition
+// function Sum(x, y) {  //named function
+//     console.log('Sum inside function', x + y);
+//     return x + y;
+// }
+
+//function expression
+// var Sum2 =
+//     function (x, y) {  // anonymous function
+//         return x + y;
+//     }
+// console.log(Sum(2, 3));
+// console.log(Sum2(22, 3));
+
+// var HOF = (cf, arg1, arg2) => {
+//     console.log(cf(arg1, arg2));
+// }
+
+// console.log(HOF((x, y) => {
+//     return x + y;
+// }, 6, 7));
+
+
